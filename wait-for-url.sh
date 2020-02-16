fcd81f9cbd215745adf495c4b2c5b43756a11db0
César Magalhães <ceanma@gmail.com>
2020-02-15 21:55:35 -0300

    Foi criado o arquivo "wait-for-url.sh"

---
+++ wait-for-url.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eux
+wait-for-url() {
+	echo "Testing $1"
+	timeout -s TERM 45 bash -c \
+	'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
+	do echo "Waiting for ${0}" && sleep 2;\
+	done' ${1}
+	echo "OK!"
+	curl -I $1
+}
