20983cf16330b502239e3689fa8047b0285c3164
César Magalhães <ceanma@gmail.com>
2020-02-24 10:18:30 -0300

    Foi recriada a variavel de ambiente HEALTHCHECK_TIMEOUT

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,10 +2,11 @@
 set -eux
 wait-for-url() {
 	echo "Testing $1"
+	timeout -s TERM $2 bash -c \
 	'while [[ "$(curl -s -o /dev/null -L -w ''%{http_code}'' ${0})" != "200" ]];\
 	do echo "Waiting for ${0}" && sleep 2;\
 	done' ${1}
 	echo "OK!"
 	curl -I $1
 }
-wait-for-url $1
+wait-for-url $1 $2
